fde088a320d66d4d02cc28d91c39378213e2dde3
tallernait <tallernait@gmail.com>
2020-01-04 23:20:02 -0500

    Listas de Personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import { Persona } from './persona.model';
 export class AppComponent {
   title = 'Listado de Personas';
   personas: Persona[] = [new Persona ("Juan", "Perez"), new Persona ("Marha" , "Gonzalez")];
-  nombreImp:string;
-  apellidoImp:string;
+  nombreInp:string;
+  apellidoInp:string;
 
   onAddPersona(){
-    let persona1 = new Persona (this.nombreImp, this.apellidoImp);
+    let persona1 = new Persona (this.nombreInp, this.apellidoInp);
     this.personas.push(persona1);
+    this.nombreInp = "";
+    this.apellidoInp = "";
   }
 }
